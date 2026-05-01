3078d8ab90e2d82ae96a892e48edc2665349be1a
ScPro <muhamadievbogdan@gmai.com>
2026-05-01 23:56:48 +0500

    update urls

--- todolist/main/urls.py
+++ todolist/main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from main import views
+from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),  # Главная страница
+    path("create/", views.create_task, name="task_create"),
+    path("update/<int:task_id>/", views.update_task, name="task_update"),
+    path("delete/<int:task_id>/", views.delete_task, name="task_delete"),
 ]
