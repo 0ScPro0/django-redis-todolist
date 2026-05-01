7aea59986718462e2f29aa6c5995529674bd4688
ScPro <muhamadievbogdan@gmai.com>
2026-05-01 22:51:08 +0500

    add redis settings

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -1,4 +1,9 @@
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv, find_dotenv
+
+find_dotenv(load_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,3 +109,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Redis settings
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
+REDIS_DB = os.getenv("REDIS_DB")
