887f0b682cf7e4ba69a5841b7a3cae0a72051e1d
ScPro <muhamadievbogdan@gmai.com>
2026-05-01 23:02:52 +0500

    fix settings

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv, find_dotenv
 
-find_dotenv(load_dotenv())
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
